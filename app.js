63a741c3253ea3bd6d9f141db3408501da735ba3
JohnnyRuss <georgiaspontoeli123@gmail.com>
2023-03-08 20:07:13 +0400

    xxx

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ App.use(cookieParser());
 
 App.use(function (req, res, next) {
   res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Origin", getOrigins()?.[0]);
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
   res.header(
     "Access-Control-Allow-Headers",
